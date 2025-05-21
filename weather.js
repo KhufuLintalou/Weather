74be34bbb7ccc860fa765a1f1a27eed92dd8c740
KhufuLintalou <148902267+KhufuLintalou@users.noreply.github.com>
2025-05-21 20:24:48 +0700

    Added function to display data

--- weather.js
+++ weather.js
@@ -24,6 +24,7 @@ async function processData(dataPromise) {
         },
     }
 
+    console.log(object);
     return object;
 }
 
@@ -31,7 +32,7 @@ async function getLocationCoord(location) {
     const apiUrl = `https://geocoding-api.open-meteo.com/v1/search?name=${location}&count=10&language=en&format=json`
     const data = await fetch(apiUrl, { mode: "cors" });
     const dataPromise = await data.json()
-    
+
     return dataPromise;
 }
 
@@ -48,8 +49,70 @@ function searchHandler(e) {
             coordData.then((coordObject) => {
                 console.log(coordObject);
             })
-        } 
+        }
     }
 }
 
 searchInput.addEventListener("keydown", searchHandler);
+
+const currentButton = document.getElementById("current");
+const dailyButton = document.getElementById("daily");
+const weatherDisplay = document.getElementById("weather-display");
+
+function emptyDisplay() {
+    while (weatherDisplay.firstChild) {
+        weatherDisplay.firstChild.remove();
+    }
+}
+
+async function displayWeatherData(lat, long) {
+    const weatherData = await processData(getWeatherData(lat, long));
+
+    if (currentButton.classList.contains("selected")) {
+        const data = document.createElement("div");
+        const mainInfo = document.createElement("div");
+        const temp = document.createElement("h1");
+        const date = document.createElement("p");
+        const codeDescript = document.createElement("p");
+
+        data.className = "data";
+        mainInfo.className = "main";
+        temp.className = "temp";
+        date.className = "date";
+        codeDescript.className = "code-desc";
+
+        temp.textContent = weatherData.current.temp;
+        date.textContent = weatherData.current.time;
+        codeDescript.textContent = weatherData.current.weatherCode;
+
+        weatherDisplay.appendChild(data);
+        data.append(mainInfo, codeDescript);
+        mainInfo.append(temp, date);
+    }
+
+    if (dailyButton.classList.contains("selected")) {
+        for (let i = 0; i < 7; i++) {
+            const data = document.createElement("div");
+            const mainInfo = document.createElement("div");
+            const temp = document.createElement("h1");
+            const date = document.createElement("p");
+            const codeDescript = document.createElement("p");
+
+            data.className = "data";
+            mainInfo.className = "main";
+            temp.className = "temp";
+            date.className = "date";
+            codeDescript.className = "code-desc";
+
+            temp.textContent = weatherData.daily.meanTemp[i];
+            date.textContent = weatherData.daily.time[i];
+            codeDescript.textContent = weatherData.daily.weatherCode[i];
+
+            weatherDisplay.appendChild(data);
+            data.append(mainInfo, codeDescript);
+            mainInfo.append(temp, date);
+        }
+    }
+}
+
+displayWeatherData(43.470861, 143.314163);
